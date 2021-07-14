2a98d8aec9607bbdd6e8259b84cf6d42a2632039
sukeshreddy54 <77787866+sukeshreddy54@users.noreply.github.com>
2021-07-14 19:48:39 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -15,3 +15,21 @@ resource "aws_instance" "web-1" {
 	CostCenter = "ABCD"
    }
 }
+
+
+LOOKUP Function
+#resource "aws_instance" "web-1" {
+#    count = 3
+#   ami = "${lookup(var.amis,var.region)}"
+#   instance_type = "t2.micro"
+#   key_name = "awswebservice"
+#   subnet_id = "${element(aws_subnet.public-subnets.*.id, count.index)}"
+#   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+#   associate_public_ip_address = true	
+#   tags = {
+#       Name = "Server-${count.index+1}"
+#       Env = "Prod"
+#       Owner = "suke"
+#	CostCenter = "ABCD"
+#   }
+#}
